44fb3f8c5579e7e6bb6a0f22612c8d1a1ce828aa
bertmad3400 <skrivtilbertram@gmail.com>
2022-04-27 12:38:32 +0200

    Use flask flatpages to render markdown posts

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 
 app.static_folder = "../static"
 app.template_folder = "../templates"
+
+DEBUG = True
+
+app.config.update(  DEBUG = DEBUG,
+                    FLATPAGES_AUTO_RELOAD = DEBUG,
+                    FLATPAGES_EXTENSION = ".md",
+                    FLATPAGES_ROOT = "../content",
+                    POST_DIR = "posts" )
+
+
+flatpages = FlatPages(app)
--- app/__main__.py
+++ app/__main__.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
 
-from app import app
+from app import app, flatpages
+
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
+@app.route("/post/")
+def listPosts():
+    print(list(flatpages))
+    posts = [p for p in flatpages if p.path.startswith(app.config["POST_DIR"])]
+    posts.sort(key=lambda item:item['date'], reverse=False)
+    return render_template('postList.html', posts=posts)
+
+@app.route('/post/<string:name>/')
+def post(name):
+    path = '{}/{}'.format(app.config["POST_DIR"], name)
+    post = flatpages.get_or_404(path)
+    return render_template('post.html', post=post)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
