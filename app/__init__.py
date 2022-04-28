c9a7060b796ea244911d3d9eb789fbba5008be25
bertmad3400 <skrivtilbertram@gmail.com>
2022-04-28 12:14:51 +0200

    Use custom flatpage renderer to evaluate jinja statements

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
-from flask import Flask
-from flask_flatpages import FlatPages
+from flask import Flask, render_template_string, Markup
+from flask_flatpages import FlatPages, pygmented_markdown
 from flask_frozen import Freezer
 
 app = Flask(__name__)
+
+def prerender_jinja(text):
+    prerendered_body = render_template_string(Markup(text))
+    return pygmented_markdown(prerendered_body)
 
 app.static_folder = "../static"
 app.template_folder = "../templates"
@@ -13,8 +17,8 @@ app.config.update(  DEBUG = DEBUG,
                     FLATPAGES_AUTO_RELOAD = DEBUG,
                     FLATPAGES_EXTENSION = ".md",
                     FLATPAGES_ROOT = "../content",
-                    POST_DIR = "posts" )
-
+                    POST_DIR = "posts",
+                    FLATPAGES_HTML_RENDERER = prerender_jinja)
 
 flatpages = FlatPages(app)
 freezer = Freezer(app)
