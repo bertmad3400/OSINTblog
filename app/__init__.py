a61e44ed906e6085843f049ae9a92b6de5d2e5ed
bertmad3400 <skrivtilbertram@gmail.com>
2022-05-22 13:17:32 +0200

    Use black to format code

--- app/__init__.py
+++ app/__init__.py
@@ -4,21 +4,25 @@ from flask_frozen import Freezer
 
 app = Flask(__name__)
 
+
 def prerender_jinja(text):
     prerendered_body = render_template_string(Markup(text))
     return pygmented_markdown(prerendered_body)
+
 
 app.static_folder = "../static"
 app.template_folder = "../templates"
 
 DEBUG = True
 
-app.config.update(  DEBUG = DEBUG,
-                    FLATPAGES_AUTO_RELOAD = DEBUG,
-                    FLATPAGES_EXTENSION = ".md",
-                    FLATPAGES_ROOT = "../content",
-                    POST_DIR = "posts",
-                    FLATPAGES_HTML_RENDERER = prerender_jinja)
+app.config.update(
+    DEBUG=DEBUG,
+    FLATPAGES_AUTO_RELOAD=DEBUG,
+    FLATPAGES_EXTENSION=".md",
+    FLATPAGES_ROOT="../content",
+    POST_DIR="posts",
+    FLATPAGES_HTML_RENDERER=prerender_jinja,
+)
 
 flatpages = FlatPages(app)
 freezer = Freezer(app)
--- app/__main__.py
+++ app/__main__.py
@@ -7,38 +7,55 @@ import sys
 
 from app import app, flatpages, freezer
 
+
 def getPage(pageName):
     return flatpages.get_or_404(f"{app.config['POST_DIR']}/{pageName}")
 
+
 @app.errorhandler(werkzeug.exceptions.HTTPException)
 def handleHTTPErrors(e):
-    return render_template("error.html", errorCode=e.code, errorName=e.name, errorDescription=e.description), e.code
+    return (
+        render_template(
+            "error.html",
+            errorCode=e.code,
+            errorName=e.name,
+            errorDescription=e.description,
+        ),
+        e.code,
+    )
+
 
 @app.route("/")
 def homepage():
     return render_template("post.html", post=getPage("homepage"))
 
+
 @app.route("/post/")
 def listPosts():
     posts = [p for p in flatpages if p.path.startswith(app.config["POST_DIR"])]
-    posts.sort(key=lambda item:item['date'], reverse=False)
-    return render_template('postList.html', posts=posts)
+    posts.sort(key=lambda item: item["date"], reverse=False)
+    return render_template("postList.html", posts=posts)
 
-@app.route('/post/<string:name>/')
+
+@app.route("/post/<string:name>/")
 def post(name):
-    return render_template('post.html', post=getPage(name))
+    return render_template("post.html", post=getPage(name))
+
 
 @app.route("/about/")
 def about():
     return render_template("post.html", post=getPage("aboutUs"))
 
-@app.route('/pygments.css')
-def pygmentsCss():
-    return pygments_style_defs('tango'), 200, {'Content-Type': 'text/css'}
 
-@app.route('/img/<string:fileName>')
+@app.route("/pygments.css")
+def pygmentsCss():
+    return pygments_style_defs("tango"), 200, {"Content-Type": "text/css"}
+
+
+@app.route("/img/<string:fileName>")
 def image(fileName):
     return send_from_directory("../static/images", fileName)
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
